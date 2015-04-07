29ac2e01beabb44f4fbe7e563b7cfd798a41a025
robstephenson <robstephenson@digital.landregistry.gov.uk>
2015-04-07 08:49:33 +0100

    alternative name added

--- api-prototype/frontend/client/gui.py
+++ api-prototype/frontend/client/gui.py
@@ -14,27 +14,34 @@ def search():
 
     forename_input = request.form['forename'];
     surname_input = request.form['surname'];
+    alternative_input = request.form['alternative']
 
 
     #  Check Inputs
-    if forename_input=="" and surname_input=="":
+    if forename_input=="" and surname_input=="" and alternative_input=="":
         #print("please complete all fields")
         forename = 'Missing forename'
         surname = 'Missing surname'
         return render_template('index.html', forename_error=forename, surname_error=surname)
-    elif forename_input=="":
+    elif forename_input=="" and alternative_input=="":
         forename = 'Missing forename'
         return render_template('index.html', forename_error=forename, surname=surname_input)
-    elif surname_input=="":
+    elif surname_input=="" and alternative_input=="":
         surname = 'Missing surname'
         return render_template('index.html', surname_error=surname, forename=forename_input)
     else:
         # Call rest service to do search
 
         url = 'http://10.0.2.2:8070/search_name'
-        data = {
-            'name': request.form['forename'] + " " + request.form['surname']
-        }
+        if alternative_input=="":
+            data = {
+                'name': request.form['forename'] + " " + request.form['surname']
+            }
+        else:
+            data = {
+                'name': request.form['alternative']
+            }
+
         headers = {'Content-Type': 'application/json'}
 
         response = requests.post(url, data=json.dumps(data), headers=headers)
