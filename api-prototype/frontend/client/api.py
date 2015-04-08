6360ae9be7db51671f70aca7fa5a9705a5081c83
robstephenson <robstephenson@digital.landregistry.gov.uk>
2015-04-08 16:06:06 +0100

    frontend for insolvency

--- api-prototype/frontend/client/api.py
+++ api-prototype/frontend/client/api.py
@@ -1,4 +1,4 @@
-from flask import Response, request
+from flask import Response, request, render_template
 import requests
 import logging
 from client import app
@@ -18,9 +18,9 @@ def remote_healthcheck():
     return Response("Recieved", status=r.status_code)
 
 
-@app.route('/b2b_register', methods=['POST'])
-def process():
-    logging.info("b2b_register called")
+@app.route('/b2b_postman', methods=['POST'])
+def postman():
+    logging.info("b2b_postman called")
     if request.headers[ 'Content-Type' ] != "application/json":
         return Response( status=415 ) # 415 (Unsupported Media Type)
     else:
@@ -41,4 +41,53 @@ def process():
         else:
             logging.error("Recieved " + response.status_code)
             return Response(response.status_code)
+
+@app.route('/insolvency', methods=['GET'])
+def insolvency():
+    logging.info("insolvency called")
+    return render_template('insolvency.html')
+
+
+@app.route('/b2b_register', methods=['POST'])
+def process():
+    logging.info("b2b_register called")
+    name_input = request.form['name']
+    nature_input = request.form['nature']
+    address_input = request.form['address']
+    name = 'Missing name'
+    nature = 'Missing nature'
+    address = 'Missing address'
+
+    #  Check Inputs
+    if name_input=="" and nature_input=="" and address_input=="":
+        #print("please complete all fields")
+        return render_template('insolvency.html', name_error=name, nature_error=nature, address_error=address)
+    elif name_input=="" and nature_input=="":
+        return render_template('insolvency.html', name_error=name, nature_error=nature)
+    elif name_input=="" and address_input=="":
+        return render_template('insolvency.html', name_error=name, address_error=address)
+    elif nature_input== "" and address_input=="":
+        return render_template('insolvency.html', nature_error=nature, address_error=address)
+    else:
+        logging.info("past if statements")
+        url = 'http://10.0.2.2:5001/register'
+        data={
+            'name': request.form['name'],
+            'address': request.form['address'],
+            'nature': request.form['nature']
+        }
+
+        print(data)
+
+        headers = {'Content-Type': 'application/json'}
+
+        response = requests.post(url, data=json.dumps(data), headers=headers)
+
+        if response.status_code == 201:
+            logging.info("Received 201")
+        else:
+            logging.error("Recieved " + response.status_code)
+            return Response(response.status_code)
+
+    return render_template('insolvency.html', results="Registration Complete")
 
