013d59373bd4e3e11e6224f9406365e2d22d6e6c
robstephenson <robstephenson@digital.landregistry.gov.uk>
2015-04-14 13:48:23 +0100

    unit tests added

--- api-prototype/database/tests/test_server.py
+++ api-prototype/database/tests/test_server.py
@@ -17,3 +17,43 @@ class TestServer:
     def test_notfound(self):
         response = self.app.get("/doesnt_exist")
         assert response.status_code == 404
+
+    def test_searchall(self):
+        response = self.app.get("/search_all")
+        assert response.status_code == 200
+
+    def test_searchname(self):
+        headers = {'Content-Type': 'application/json'}
+        name = '{"name": "ROBERT TREBOR"}'
+        response = self.app.post('/search_name', data=name, headers=headers)
+        assert response.status_code == 200
+        assert 'FLAT' in response.data.decode()
+        print(response.data.decode())
+
+    def test_searchloop(self):
+        data = [
+            '{"name": "ROBERT TREBOR"}',
+            '{"name": "Jack Bloggs"}',
+            '{"name": "pat smith"}'
+        ]
+        headers = {'Content-Type': 'application/json'}
+        num = 0
+        for i in data:
+            name = data[num]
+            response = self.app.post('/search_name', data=name, headers=headers)
+            assert response.status_code == 200
+            print(response.data.decode())
+            num = num + 1
+
+    def test_register(self):
+        headers = {'Content-Type': 'application/json'}
+        data = '{"nature": "WOB", "name": "I.P. Freely", "address": "54 high street, plymouth"}'
+        response = self.app.post('/register', data=data, headers=headers)
+        assert response.status_code == 201
+        print(response.data.decode())
+
+
+
+
+
+
--- api-prototype/frontend/tests/test_b2b.py
+++ api-prototype/frontend/tests/test_b2b.py
@@ -1,9 +1,50 @@
 import pytest
-import mock
-from service.server import app
+from unittest import mock
+from client.api import app
+import requests
+
+class FakeResponse(requests.Response):
+    def __init__(self, content='', status_code=200):
+        super(FakeResponse, self).__init__()
+        self._content = content
+        self._content_consumed = True
+        self.status_code = status_code
+
+search_string = '[{"nature": "PAB", "id": 202, "address": "12 HIGH STREET", "date": "02/04/2015", "name": "JACK BLOGGS"}]'
+
+fake_error = FakeResponse('Wibble', 500)
+fake_success = FakeResponse('Wibble', 200)
+fake_search = FakeResponse(str.encode(search_string), 200)
 
 
 class TestB2B:
     def setup_method(self, method):
         self.app = app.test_client()
+
+    @mock.patch('requests.get', return_value=fake_success)
+    def test_remote_healthcheck(self, mock_get):
+        response = self.app.get("/remote")
+        assert response.status_code == 200
+
+    @mock.patch('requests.post', return_value=fake_search)
+    def test_search(self, mock_post):
+        response = self.app.post("/search",
+                                 data={'forename': 'Jack', 'surname': 'Bloggs', 'alternative': ''})
+        assert('JACK BLOGGS' in response.data.decode())
+        assert('12 HIGH STREET' in response.data.decode())
+
+    fake_search = FakeResponse(str.encode(search_string), 201)
+    @mock.patch('requests.post', return_value=fake_search)
+    def test_b2bregister(self, mock_post):
+        response = self.app.post('/b2b_register', data={'nature': 'WOB', 'name': 'John Forename', 'address': '12 high street'})
+        assert response.status_code == 200
+
+    @mock.patch('requests.post', return_value=fake_search)
+    def test_b2bpostman(self, mock_post):
+        headers = {'Content-Type': 'application/json'}
+        response = self.app.post('/b2b_postman', data='{"nature": "WOB", "name": "John Forename", "address": "12 high street"}', headers=headers)
+        assert response.status_code == 201
+
+
+
 
